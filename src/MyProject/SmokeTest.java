f5d3ee0403a72a6611341e2c5054963d718642cc
islamManzoor <47377484+islamManzoor@users.noreply.github.com>
2019-02-22 14:50:27 +0500

    Wait added.

--- src/MyProject/SmokeTest.java
+++ src/MyProject/SmokeTest.java
@@ -5,6 +5,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class SmokeTest 
